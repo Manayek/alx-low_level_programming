3a4fb93fe81c9b489484e2cb14aecc8bad95c96a
Manayek <manayekerebn7@gmail.com>
2022-09-08 11:12:43 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * * main - Print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * *                followed by a new line, to standard error.
  * *
  * * Return: Always 1.
@@ -10,7 +10,7 @@
 int main(void)
 {
 		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 9);
+					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 			return (1);
 }

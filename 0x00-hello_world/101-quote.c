5524117a730f1726ca92c434c349e76f98c1f106
Manayek <manayekerebn7@gmail.com>
2022-09-08 11:22:05 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,7 @@
 #include <unistd.h>
 
 /**
- * * main - Print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * *                followed by a new line, to standard error.
- * *
+ * * main - Prints out the last part of a quote in the standard error.
  * * Return: Always 1.
  * *
  */

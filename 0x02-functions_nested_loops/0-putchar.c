78b7319756540aeef28864b7b5a0e27af3f77bd0
Manayek <manayekerebn7@gmail.com>
2022-09-13 11:07:51 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,18 @@
 #include "main.h"
-
-
 /**
- * * main - Entry point
+ * * main - check the code
  * *
- * * Return: nothing, return void
+ * * Return: Always 0.
  * *
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	char c[] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
 	return (0);
 }

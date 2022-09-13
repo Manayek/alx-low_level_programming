3b5ef18bd1d86a39b2d92c0e5f948f2ea725d13a
Manayek <manayekerebn7@gmail.com>
2022-09-13 04:44:25 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * * main - Entry Point.use only user defined variable to create output.
+ * *
+ * * Description: Print _putchar using predefined functions
+ * *
+ * * Return: 0
+ * */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}

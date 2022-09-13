ccb065f9d89a5cc75f6f736ab8118e976b8f6cfe
Manayek <manayekerebn7@gmail.com>
2022-09-13 10:12:35 -0700

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,20 +4,23 @@
  * *
  * * Return: x10 a-z
  * *
- *  /
- *  void print_alphabet_x10(void)
- *  {
- *  int n, ma;
- *
- *  ma=0;
- *
- *  while(ma < 10)
- *  {
- *  for (n = 'a'; n <= 'z'; n++)
- *  {
- *  _putchar(n);
- *  }
- *  ma++;
- *  _putchar('\n');
- *  }
- *  }
+ */
+int main(void)
+{
+	void print_alphabet_x10(void)
+	{
+		int n, ma;
+
+		ma=0;
+
+		while(ma < 10)
+		{
+			for (n = 'a'; n <= 'z'; n++)
+			{
+				_putchar(n);
+			}
+			ma++;
+			_putchar('\n');
+		}
+	}
+}

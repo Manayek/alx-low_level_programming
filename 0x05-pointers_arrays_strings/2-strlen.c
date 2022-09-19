79702dec40b4f598f8488e091feafab0af600203
Manayek <manayekerebn7@gmail.com>
2022-09-19 08:47:10 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,16 +5,16 @@
  * *
  * *Return: length of a string.
  * *
- *  /
- *  int _strlen(char *s)
- *  {
- *  int i;
- *
- *  i = 0;
- *  while (*s != '\0')
- *  {
- *  i++;
- *  s++;
- *  }
- *  return (i);
- *  }
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}

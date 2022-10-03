1ec8a69810fe462b555617c3b77a4316d26f8577
Manayek <manayekerebn7@gmail.com>
2022-10-03 03:52:46 -0700

    advanced

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -59,8 +59,10 @@ char **strtow(char *str)
 			{
 				end = i;
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL);
-						return (NULL);
+				if (tmp == NULL)
+
+				return (NULL);
+
 				while (start < end)
 
 						*tmp++ = str[start++];

ee3e2250c2c60130ea76f59228d073674fe267c3
Manayek <manayekerebn7@gmail.com>
2022-11-01 04:51:43 -0700

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
@@ -34,6 +33,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
-
 	return (1);
 }

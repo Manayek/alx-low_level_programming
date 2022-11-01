f281c15051506786cfd893969b0099abeb633854
Manayek <manayekerebn7@gmail.com>
2022-11-01 05:54:32 -0700

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include "main.h"
 
 /**
  *
  * * create_file - function that creates a file.
- *
  * * @filename: variable pointer
- *
  * * @text_content: content file
- *
  * * Description: Create a function that creates a file.
- *
  * * Return: 1 on success, -1 on failure
- *
  * *
  */
 
